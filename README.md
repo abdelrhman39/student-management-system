073200446dfba1adc33573457ce4485ee6a05c51
Abdelrhman Atwa <42987724+abdelrhman39@users.noreply.github.com>
2025-01-26 22:20:25 +0200

    first commit

--- README.md
+++
@@ -1,4 +0,0 @@
-
-## Laravel Student Management System (LSMS)
-
-<p>This is simple student management system. This project has also include Laravel Role Permission System (spatie)</p>
--- resources/views/adminstration/permission/element.blade.php
+++
@@ -1,9 +0,0 @@
-
-@include('partials.flash_messages.flashMessages')
-
-<div class="form-group">
-    <label class="col-lg-2 control-label">Name<span class="required-star"> *</span></label>
-    <div class="col-lg-10">
-        <input value="{{ isset($permission->name) ? $permission->name:'' }}" required="required" name="name" type="text" class="form-control">
-    </div>
-</div>
